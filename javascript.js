323df2c5ff1964df5b60d42fce7ba3e42d9820b0
jordangonen <jordangonen@wustl.edu>
2017-11-19 15:37:47 -0600

    hmmm

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 
 //elementSwapper
 function swapElement(hide, show) {
+  console.log(hide);
+  console.log(show);
   document.getElementById(hide).style.display = 'none';
   document.getElementById(show).style.display = 'block';
   currentView = show;
+  // console.log(currentView);
+  console.log(document.getElementById("weather-modal").style.display);
 }
 document.getElementById("iterate").addEventListener("click", function() {
-  swapElement(arrow);
-
-
+  console.log(document.getElementById("weather-modal").style.display);
+  if(document.getElementById("weather-modal").style.display = 'block'){
+    console.log('weather');
+    swapElement("weather-modal", "calendar-modal");
+  }
+  else if(document.getElementById("calendar-modal").style.display = 'block'){
+    console.log('calendar');
+    swapElement("calendar-modal", "weather-modal");
+  }
 });
