96793bcb81c9a32fc7fb55ceebfb608977549c2a
jordangonen <jordangonen@wustl.edu>
2017-11-19 16:52:44 -0600

    check it

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
+var currentView = "weather-modal";
+document.getElementById("iterate").addEventListener("click", function() {
+  if(currentView = "weather-modal"){
+    swapElement("weather-modal", "calendar-modal");
+  } else if(currentView = "calendar-modal"){
+    swapElement("calendar-modal", "weather-modal");
+  }
+});
+
 
 //elementSwapper
 function swapElement(hide, show) {
   console.log(hide);
-  console.log(show);
   document.getElementById(hide).style.display = 'none';
   document.getElementById(show).style.display = 'block';
   currentView = show;
-  // console.log(currentView);
-  console.log(document.getElementById("weather-modal").style.display);
 }
-currentView = "calendar-modal";
-document.getElementById("iterate").addEventListener("click", function() {
-  console.log(document.getElementById("weather-modal").style.display);
-  if(currentView = "weather-modal"){
-    swapElement("weather-modal", "calendar-modal");
-  }
-  else if(currentView = "calendar-modal"){
-    swapElement("calendar-modal", "weather-modal");
-  }
+
+document.getElementById("settings").addEventListener("click", function() {
+  // popup
 });
