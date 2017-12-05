e25004e974926e036b935fe43c1d3954070c2c24
justin friedman <justinfriedman22@gmail.com>
2017-12-04 22:27:49 -0600

    zip print works

--- src/API/magicMirrorHWAPI.cpp
+++ src/API/magicMirrorHWAPI.cpp
@@ -3,9 +3,7 @@
 const int TFT_CS = A2;
 const int TFT_DC = DAC;
 const int TFT_RST = -1;
-int onDeck = 0;
-int done = 0;
-int sent = 0;
+
 int zip = 00000;
 String spotSong = "";
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
@@ -22,48 +20,12 @@ int zipSetter(String command)
     }
 
   }
-  else return -1;
-}
-
-void trelloNewOnDeck(const char *event, const char *data){
-  Serial.println("ondeck ran");
-    onDeck = onDeck + 1;
-
-    if (state == 0) {
-        FSM(state);
-    }
-
-
-}
-void trelloNewDone(const char *event, const char *data) {
-  Serial.println("done ran");
-  done = done+1;
-
-  if (state == 0) {
-      FSM(state);
+  else {
+    return -1;
   }
-
 }
-void gmailNewSent (const char *event, const char *data) {
-  Serial.println("gmail ran");
-  sent = sent + 1;
-
-  if (state == 1) {
-      FSM(state);
-  }
 
 
-}
-void spotifyNewSave (const char *event, const char *data) {
-  Serial.println("spotufy ran");
-    spotSong = String(data);
-
-    if (state == 2) {
-        FSM(state);
-    }
-
-
-}
 
 
 
@@ -75,10 +37,7 @@ void apiSetup() {
   tft.begin(HX8357D);
   tft.setRotation(1);
   // Subscribe to the integration response event
-  Particle.subscribe("trello/newOnDeck", trelloNewOnDeck);
-  Particle.subscribe("gmail/newSent", gmailNewSent);
-  Particle.subscribe("spotify/newSave", spotifyNewSave);
-  Particle.subscribe("trello/newDone", trelloNewDone);
+
   tft.fillScreen(HX8357_BLACK);
   tft.setTextSize(5);
    Particle.function("zipSetter", zipSetter);
@@ -88,47 +47,6 @@ void apiSetup() {
 //screen setup
 //fsm
 
-void FSM (int arg) {
-  switch(arg) {
-    case 0 : //trello
-      tft.fillScreen(HX8357_BLACK);
-      tft.setCursor(0,0);
-      tft.print("Trello Progress");
-      tft.print('\n');
-      tft.print("On Deck: ");
-      tft.print(onDeck);
-      tft.print('\n');
-      tft.print("Done: ");
-      tft.print(done);
-      Serial.println("reqrote screen");
-
-
-      break;
-    case 1 : // gmail
-    tft.fillScreen(HX8357_BLACK);
-    tft.setCursor(0,0);
-    tft.print("gmails sent today: ");
-    tft.print(sent);
-
-    Serial.println("reqrote screen");
-
-
-    break;
-    case 2 : //spotify
-    tft.fillScreen(HX8357_BLACK);
-    tft.setCursor(0,0);
-    tft.print("Last Saved Spotify");
-    tft.print('\n');
-    tft.print(spotSong);
-
-    Serial.println("reqrote screen");
-
-
-    break;
-  }
-
-
-}
 
 
 
--- src/IoT_SmartMirror.ino
+++ src/IoT_SmartMirror.ino
@@ -6,19 +6,67 @@
  */
 
  #include "API/magicMirrorHWAPI.h"
+ #include <ArduinoJson.h>
+
+
 
 
  void setup() {
-     //API setup function
-     apiSetup();
-    //  FSM(1);
+   Serial.begin(9600);
+   Particle.subscribe("hook-response/getCurrentWeather/0", currentWeatherHandler, MY_DEVICES);
+   Particle.function("zipPost", zipPost);
 
 
  }
 
  void loop() {
-    //API screen write
-    //API distance get????
 
-      
+   Particle.publish("getCurrentWeather", "10583");
+   Serial.print("looped");
+   delay(5000);
+
+ }
+
+ void currentWeatherHandler(const char *event, const char *data) {
+   // Handle the webhook response
+   String str = String(data);
+
+
+      // Serial.println(str);
+
+
+     char json[500] = "";
+     str.toCharArray(json, 500);
+    //  Serial.println(json);
+
+
+
+
+    StaticJsonBuffer<500> jsonBuffer;
+
+    //  int temp = atoi(strtok(json, "\"~"));
+    //   String condition = strtok(NULL, "~");
+    //   String name = strtok(NULL, "~");
+    //   Serial.println(temp);
+
+
+
+   JsonObject& root = jsonBuffer.parseObject(json);
+   if (!root.success()){
+    Serial.println("parseObject() failed");
+    return;
+  }
+  const char* name    = root["name"];
+  const char* condition  = root["condition"];
+  double temp = root["temp"];
+  double hum = root["hum"];
+  double windS = root["windS"];
+  double windD = root["windD"];
+
+ }
+
+ int zipPost(String command) {
+   Serial.println(command);
+
+
  }
